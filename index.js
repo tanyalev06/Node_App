a18a74a1c46251e54f311009b66aa1c1d880f301
tanyalev06 <grib.tanya06@mail.ru>
2022-07-10 23:25:20 +0300

    modules os http event nodemon

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const obj = require(('./user'));
+const http = require('http');
 
-console.log(obj.user);
+const server = http.createServer((req, res) => {
+console.log(req.url)
 
-obj.sayHello();
+res.write('<h1>Hello from</h1>')
+res.write('<h2>Hello from</h2>')
+res.write('<h3>Hello from</h3>')
+res.end(`
+    <div style="background: red; width: 200px; height: 200px;">
+    <h1>Test 1</h1>
+    </div>
+    `) //закрыть ответ
+});
+
+//запускаем сервер
+server.listen(3000, () => {
+    console.log('Server is running')
+});
---
+++ refs/os_refs.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+//Платформа
+console.log(os.platform());
+
+//Архитектура
+console.log(os.arch());
+
+//Информация о комп.
+console.log(os.cpus());
+
+//Свободная память
+console.log(os.freemem());
+
+//Сколько всего памяти
+console.log(os.totalmem());
+
+//Корневая директоия
+console.log(os.homedir());
+
+//сколько система работает
+console.log(os.uptime());
